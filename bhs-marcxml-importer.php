0687d9772830fe81b95c160ab29ab888b99f47ed
anarchivist <anarchivist@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-14 04:56:47 +0000

    Changing name; plus basis for marc xml parser
    git-svn-id: http://plugins.svn.wordpress.org/bhs-marcxml-importer/trunk@396936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bhs-marcxml-importer.php
+++ bhs-marcxml-importer.php
@@ -13,7 +13,7 @@ License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 require_once('File/MARCXML.php');
 
 /**
- * BHS MARC Importer
+ * BHS MARCXML Importer
  *
  * @package WordPress
  * @subpackage Importer
